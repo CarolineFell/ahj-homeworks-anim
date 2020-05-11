8233dd466cda0c5e5bbe937f63b95c65a9a13719
Gradt <carolinefell@yandex.ru>
2020-05-11 10:42:40 +0300

    Collapse - Callback Chat - Liker

---
+++ src/js/Liker.js
@@ -0,0 +1,20 @@
+/* eslint-disable class-methods-use-this */
+export default class Liker {
+  init() {
+    const likerButton = document.querySelector('.button-liker');
+    const image = document.querySelector('.image');
+
+    likerButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      const randomImage = Math.floor(Math.random() * 4) + 1;
+      const likerImage = document.createElement('img');
+      likerImage.src = 'heart.png';
+      likerImage.className = 'liker-image';
+      likerImage.style.animationName = `heart${randomImage}`;
+      image.appendChild(likerImage);
+      likerImage.addEventListener('animationend', () => {
+        image.removeChild(likerImage);
+      });
+    });
+  }
+}
